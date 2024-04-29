eadeb5ca8b1c7b6f3092cd22b3f7cf85c4610f39
osama2 <osama0.syria@gmail.com>
2024-04-30 00:04:33 +0300

    i removed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/osama-repo/',
+  base: '/osama-repo',
   plugins: [svgr(), react()],
 });
